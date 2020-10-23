426302ae31f8068b207363cef37ab5874305fa49
Amir Boroufar <41783757+aboroufar@users.noreply.github.com>
2020-10-23 13:19:17 +0200

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,8 @@
 
 set -xe
 
-METAL3_DIR="/root/kube-cluster/metal3-dev-env"
+#METAL3_DIR="/root/kube-cluster/metal3-dev-env"
+METAL3_DIR="$(dirname "$(readlink -f "${0}")")/.."
 
 ACTION="${ACTION:-""}"
 
@@ -32,7 +33,7 @@ source "${METAL3_DIR}/lib/images.sh"
 export ANSIBLE_HOST_KEY_CHECKING=False
 echo "${METAL3_DIR}/vm-setup/v1aX_integration_test.yml"
 ANSIBLE_FORCE_COLOR=true ansible-playbook \
-   -e "metal3_dir=$METAL3_DIR" \
+   -e "metal3_dir=$SCRIPTDIR" \
    -e "v1aX_integration_test_action=${ACTION}" \
    -i "${METAL3_DIR}/vm-setup/inventory.ini" \
    -b -vvv "${METAL3_DIR}/vm-setup/v1aX_integration_test.yml"
