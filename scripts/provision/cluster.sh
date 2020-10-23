1186c0ada0d787a41a937810961d5915020007b0
Amir Boroufar <41783757+aboroufar@users.noreply.github.com>
2020-10-23 13:17:25 +0200

    Update cluster.sh

--- scripts/provision/cluster.sh
+++ scripts/provision/cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-METAL3_DIR="/root/kube-cluster/metal3-dev-env"
+#METAL3_DIR="/root/kube-cluster/metal3-dev-env"
+METAL3_DIR="$(dirname "$(readlink -f "${0}")")/../.."
 
 export ACTION="provision_cluster"
 
